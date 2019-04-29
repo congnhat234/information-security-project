1245a601584845c4d316e7b7209a706408ba272a
congnhat234 <31690712+congnhat234@users.noreply.github.com>
2019-04-29 20:57:20 +0700

    First commit

---
+++ probe_req2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from scapy.all import *
+def PacketHandler(pkt) :
+	if pkt.haslayer(Dot11):
+		if pkt.type == 0 and pkt.subtype == 4 :
+			if pkt.info :
+				print "Client: %s SSID: %s" % (pkt.addr2, pkt.info)
+			
+sniff(iface= "wlan0mon",prn=PacketHandler)
